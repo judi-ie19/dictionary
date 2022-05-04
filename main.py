e6013c7b28e274e955dd6a2da5afc444a25e349e
jud-ie19 <owuorakinyijudith@gmail.com>
2022-05-04 10:58:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+student ={'name':'Amina','age':23,'codenumber':['037']}
+print(student['name'])
+print(student['age'])
+print(student['codenumber'])
+print(student.get('phonenumber'))
+student['phone']='0757497424'
+student['name']='Judith'
+print(student)
